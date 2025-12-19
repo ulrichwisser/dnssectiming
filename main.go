6bc472255af046bb71ee7891d69ef9a9b962ca39
Ulrich Wisser <ulrich@wisser.se>
2025-12-19 14:40:05 +0100

    Init logging as early as possible

--- main.go
+++ main.go
@@ -4,6 +4,19 @@ import (
 	"github.com/ulrichwisser/dnssectiming/cmd"
 )
 
+import (
+	"os"
+
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/logfmt"
+)
+
+func init() {
+	// Set default log handler
+	log.SetHandler(logfmt.New(os.Stderr))
+	log.SetLevel(log.DebugLevel)
+}
+
 func main() {
 	cmd.Execute()
 }
